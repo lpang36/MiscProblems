78ef4648bd1f8868d36a1f0c54eee954349b3f46
Lawrence Pang <lawrencepang36@gmail.com>
2017-09-03 14:42:44 -0400

    shell script to create java files automatically

--- InitJava.sh
+++ InitJava.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 if [ "$#" -eq 2 ]; then
     cd $1
-    #do stuff
+    echo "public class $2 {
+	public $2 () {
+	}
+	public static void main(String[] args) {
+		$2 obj = new $2();
+	}
+}" >> $2.java
+    notepadqq $2.java
     cd ..
 fi
 if [ "$#" -eq 1 ]; then
     #do stuff
+    echo "public class $1 {
+	public $1 () {
+	}
+	public static void main(String[] args) {
+		$1 obj = new $1();
+	}
+}" >> $1.java
+    notepadqq $1.java
 fi
